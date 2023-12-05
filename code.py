cde13f50b2b5958e7c4a78bea066463baff177d2
Impetuosity <113357851+Impetuosity@users.noreply.github.com>
2023-12-05 20:45:19 +0300

    Update code.py
    UI aded

--- code.py
+++ code.py
@@ -6,18 +6,26 @@ if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((1920, 1080))
     pygame.display.flip()
-    pygame.draw.rect(screen, (255, 255, 255), (651, 409, 522, 100), 5, 20)
-    f1 = pygame.font.SysFont("Fredoka", 200)
-    text1 = f1.render('Just Survive', False,
+    for i in range(3):
+        pygame.draw.rect(screen, (255, 255, 255), (751, 409 + i * 150, 422, 100), 5, 20)
+    name = pygame.font.SysFont(None, 200).render('Just Survive', False,
                   (255, 255, 255))
-    f2 = pygame.font.SysFont('Fredoka', 128)
-    text2 = f2.render("Start", False,
+    screen.blit(name, (555, 76))
+
+    start_btn = pygame.font.SysFont(None, 128).render("Start", False,
                   (255, 255, 255))
-    screen.blit(text1, (505, 76))
-    screen.blit(text2, (701, 421))
+    screen.blit(start_btn, (851, 421))
+
+    res_btn = pygame.font.SysFont(None, 128).render("Results", False,
+                  (255, 255, 255))
+    screen.blit(res_btn, (799, 571))
+
+    quit_btn = pygame.font.SysFont(None, 128).render("Quit", False,
+                  (255, 255, 255))
+    screen.blit(quit_btn, (867, 721))
+
     pygame.display.update()
     while pygame.event.wait().type != pygame.QUIT:
         pass
     pygame.quit()
-    
     
