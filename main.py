4edd2aaf92a56031c85b23b72b519d590f0b47c9
dan2400 <dan24_00@mail.ru>
2023-12-15 17:32:36 +0300

    collides and mouse

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 import math
 
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        self.add(enemies)
+        self.image = pygame.Surface([1, y2 - y1])
+        self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -12,11 +19,14 @@ class Player(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, pygame.Color("black"),
                            (self.radius, self.radius), self.radius)
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[1]
-        self.player_speed = 5
+        self.player_speed = 6
 
     def update(self):
+        if pygame.sprite.spritecollideany(self, enemies):
+            print('colides')
         mouse_x, mouse_y = pygame.mouse.get_pos()
         dx = mouse_x - self.rect.centerx
         dy = mouse_y - self.rect.centery
@@ -26,37 +36,50 @@ class Player(pygame.sprite.Sprite):
         else:
             self.rect.center = pygame.mouse.get_pos()
 
-class Play:
-    def __init__(self):
-        self.hard = 1
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+Border(0, 0, 1920, 0)
+Border(0, 1920, 1080, 1920)
+Border(0, 0, 0, 1080)
+Border(1920, 0, 1920, 1080)
+player = Player()
+all_sprites.add(player)
 
 
-if __name__ == '__main__':
-    enemies = pygame.sprite.Group()
-    all_sprites = pygame.sprite.Group()
-    player = Player()
-    all_sprites.add(player)
-    X = 1920
-    Y = 1080
-    screen = pygame.display.set_mode((X, Y))
-    screen.fill((255, 255, 255))
-    pygame.init()
-    running = True
-    pause = False
-    clock = pygame.time.Clock()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    if pause:
-                        pause = False
-                    else:
-                        pause = True
-        all_sprites.update()
-        all_sprites.draw(screen)
-        pygame.display.flip()
+def play():
+    if __name__ == '__main__':
+        mouse_cursor = pygame.Surface((35 * 2, 35 * 2), pygame.SRCALPHA, 32)
+        pygame.draw.circle(mouse_cursor, pygame.Color("black"),
+                           (35, 35), 35, 3)
+        X = 1920
+        Y = 1080
+        screen = pygame.display.set_mode((X, Y))
         screen.fill((255, 255, 255))
-    clock.tick(120)
-pygame.quit()
+        pygame.mouse.set_visible(False)
+        pygame.init()
+        running = True
+        pause = False
+        clock = pygame.time.Clock()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        if pause:
+                            pause = False
+                        else:
+                            pause = True
+            x, y = pygame.mouse.get_pos()
+            screen.blit(mouse_cursor, (x - 35, y - 35))
+            all_sprites.update()
+            all_sprites.draw(screen)
+            pygame.display.flip()
+            screen.fill((255, 255, 255))
+        clock.tick(120)
+    pygame.quit()
+
+
+play()
